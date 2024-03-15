37aaf4dfd27d19a911d97b7fb7a4d5ae3280e29a
SYeon0129 <wkfkwkd00@naver.com>
2024-03-15 11:15:54 +0900

    깃올리기 연습1

---
+++ abc/src/main/java/testabc/ABCTest.java
@@ -0,0 +1,9 @@
+package testabc;
+
+public class ABCTest {
+
+	public static void main(String[] args) {
+		System.out.println("깃 연습");
+	}
+
+}
---
+++ abc/src/main/java/testabc/copy/ABCTest.java
@@ -0,0 +1,9 @@
+package testabc.copy;
+
+public class ABCTest {
+
+	public static void main(String[] args) {
+		System.out.println("깃 연습");
+	}
+
+}
